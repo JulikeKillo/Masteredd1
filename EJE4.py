539c9bbb1b26ab4e0c081f20fdd938609292a5ca
Julike <julianerrasti99@gmail.com>
2021-11-10 17:07:52 +0100

    mi primer commit#

---
+++ EJE4.py
@@ -0,0 +1,12 @@
+CC = str(input('crear clave...'))
+AC = str(input(''))
+if AC != CC:
+    print('no concuerda repetir')
+else:
+    print('Bienvenido...')
+Pass = str('casa123')
+En_Pass = str(input('contraseña: '))
+if Pass != En_Pass.lower():
+    print('no son iguales...')
+else:
+    print('bienvenido')
